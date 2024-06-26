30fa607a84c9a8624d1b5ec9591914350dbf4b0a
David Freitas <davidjumi51@gmail.com>
2024-06-26 16:29:47 -0300

    Forgot your password?

---
+++ IronGym.Shared/Entities/ForgotYourPasswordEmail.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IronGym.Shared.Entities
+{
+    public class ForgotYourPasswordEmail
+    {
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
+        public string Email { get; set; }
+    }
+}
---
+++ MVC/Controllers/ForgotYourPassword.cs
@@ -0,0 +1,20 @@
+﻿using IronGym.Shared.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVC.Controllers
+{
+    public class ForgotYourPassword : Controller
+    {
+        private readonly AESService _aesService;
+        public ForgotYourPassword(AESService aesService)
+        {
+            _aesService = aesService;
+        }
+
+        [HttpGet]
+        public IActionResult AddEmail()
+        {
+            return View();
+        }
+    }
+}
