ed787377b215a799ac28bd60107b4784c7996df1
David Freitas <davidjumi51@gmail.com>
2024-07-01 10:10:01 -0300

    Better e-mail body

--- Application/Services/EmailService.cs
+++ Application/Services/EmailService.cs
@@ -45,146 +45,150 @@ namespace IronGym.Application.Services
         {
             MailMessage message = new MailMessage();
             message.To.Add(userEmail);
-            message.From = new MailAddress("verifylockbox@gmail.com");
-
+            message.From = new MailAddress("verifyirongym@gmail.com");
 
             message.Body = $@"
-            <html>
-            <head>
-                <style>
-                    .email-container {{
-                        font-family: Arial, sans-serif;
-                        line-height: 1.6;
-                        color: #333333;
-                        padding: 20px;
-                        max-width: 600px;
-                        margin: auto;
-                    }}
-                    .email-header {{
-                        background-color: #f7f7f7;
-                        padding: 10px 20px;
-                        border-bottom: 1px solid #dddddd;
-                    }}
-                    .email-header h2 {{
-                        margin: 0;
-                    }}
-                    .email-content {{
-                        padding: 20px;
-                        background-color: #ffffff;
-                        border: 1px solid #dddddd;
-                    }}
-                    .verification-code {{
-                        font-size: 1.5em;
-                        color: #4CAF50;
-                        font-weight: bold;
-                        margin: 20px 0;
-                    }}
-                    .email-footer {{
-                        padding: 10px 20px;
-                        background-color: #f7f7f7;
-                        border-top: 1px solid #dddddd;
-                        text-align: center;
-                        font-size: 0.9em;
-                        color: #777777;
-                    }}
-                </style>
-            </head>
-            <body>
-                <div class='email-container'>
-                    <div class='email-header'>
-                        <h2>Lockbox Verification</h2>
-                    </div>
-                    <div class='email-content'>
-                        <p>Dear User,</p>
-                        <p>Thank you for using Lockbox! To complete your verification process, please use the verification code provided below:</p>
-                        <p class='verification-code'>{verificationCode}</p>
-                        <p>If you did not request this verification code, please ignore this email or contact our support team.</p>
-                        <p>Best regards,<br>Lockbox Team</p>
-                    </div>
-                    <div class='email-footer'>
-                        &copy; 2024 Lockbox. All rights reserved.
-                    </div>
-                </div>
-            </body>
-            </html>";
+    <html>
+    <head>
+        <style>
+            .email-container {{
+                font-family: Arial, sans-serif;
+                line-height: 1.6;
+                color: #333333;
+                padding: 20px;
+                max-width: 600px;
+                margin: auto;
+            }}
+            .email-header {{
+                background-color: #000000;
+                padding: 10px 20px;
+                border-bottom: 1px solid #dddddd;
+            }}
+            .email-header h2 {{
+                margin: 0;
+                color: #f36100;
+            }}
+            .email-content {{
+                padding: 20px;
+                background-color: #000000;
+                border: 1px solid #dddddd;
+                color: #ffffff;
+            }}
+            .verification-code {{
+                font-size: 1.5em;
+                color: #f36100;
+                font-weight: bold;
+                margin: 20px 0;
+            }}
+            .email-footer {{
+                padding: 10px 20px;
+                background-color: #000000;
+                border-top: 1px solid #dddddd;
+                text-align: center;
+                font-size: 0.9em;
+                color: #ffffff;
+            }}
+        </style>
+    </head>
+    <body>
+        <div class='email-container'>
+            <div class='email-header'>
+                <h2>IronGym Verification</h2>
+            </div>
+            <div class='email-content'>
+                <p>Dear User,</p>
+                <p>Thank you for using IronGym! To complete your verification process, please use the verification code provided below:</p>
+                <p class='verification-code'>{verificationCode}</p>
+                <p>If you did not request this verification code, please ignore this email or contact our support team.</p>
+                <p>Best regards,<br>IronGym Team</p>
+            </div>
+            <div class='email-footer'>
+                &copy; 2024 IronGym. All rights reserved.
+            </div>
+        </div>
+    </body>
+    </html>";
             message.IsBodyHtml = true;
 
-
-            message.Subject = "LockBox Verification Code";
+            message.Subject = "IronGym Verification Code";
             return message;
         }
-        public static MailMessage WritePasswordEmail(string userEmail, string verificationCode)
+
+
+        public static MailMessage WritePasswordEmail(string userEmail, string password)
         {
             MailMessage message = new MailMessage();
             message.To.Add(userEmail);
-            message.From = new MailAddress("verifylockbox@gmail.com");
-
+            message.From = new MailAddress("verifyirongym@gmail.com");
 
             message.Body = $@"
-            <html>
-            <head>
-                <style>
-                    .email-container {{
-                        font-family: Arial, sans-serif;
-                        line-height: 1.6;
-                        color: #333333;
-                        padding: 20px;
-                        max-width: 600px;
-                        margin: auto;
-                    }}
-                    .email-header {{
-                        background-color: #f7f7f7;
-                        padding: 10px 20px;
-                        border-bottom: 1px solid #dddddd;
-                    }}
-                    .email-header h2 {{
-                        margin: 0;
-                    }}
-                    .email-content {{
-                        padding: 20px;
-                        background-color: #ffffff;
-                        border: 1px solid #dddddd;
-                    }}
-                    .verification-code {{
-                        font-size: 1.5em;
-                        color: #4CAF50;
-                        font-weight: bold;
-                        margin: 20px 0;
-                    }}
-                    .email-footer {{
-                        padding: 10px 20px;
-                        background-color: #f7f7f7;
-                        border-top: 1px solid #dddddd;
-                        text-align: center;
-                        font-size: 0.9em;
-                        color: #777777;
-                    }}
-                </style>
-            </head>
-            <body>
-                <div class='email-container'>
-                    <div class='email-header'>
-                        <h2>Lockbox Verification</h2>
-                    </div>
-                    <div class='email-content'>
-                        <p>Dear Employee,</p>
-                        <p>Welcome do the IronGym team! Your password is provided below:</p>
-                        <p class='verification-code'>{verificationCode}</p>
-                        <pLogin using this password and the e-mail you recieved it.</p>
-                        <p>Best regards,<br>IronGym Team</p>
-                    </div>
-                    <div class='email-footer'>
-                        &copy; 2024 IronGym. All rights reserved.
-                    </div>
-                </div>
-            </body>
-            </html>";
+    <html>
+    <head>
+        <style>
+            .email-container {{
+                font-family: Arial, sans-serif;
+                line-height: 1.6;
+                color: #333333;
+                padding: 20px;
+                max-width: 600px;
+                margin: auto;
+            }}
+            .email-header {{
+                background-color: #000000;
+                padding: 10px 20px;
+                border-bottom: 1px solid #dddddd;
+            }}
+            .email-header h2 {{
+                margin: 0;
+                color: #f36100;
+            }}
+            .email-content {{
+                padding: 20px;
+                background-color: #000000;
+                border: 1px solid #dddddd;
+                color: #ffffff;
+            }}
+            .password {{
+                font-size: 1.5em;
+                color: #f36100;
+                font-weight: bold;
+                margin: 20px 0;
+            }}
+            .email-footer {{
+                padding: 10px 20px;
+                background-color: #000000;
+                border-top: 1px solid #dddddd;
+                text-align: center;
+                font-size: 0.9em;
+                color: #ffffff;
+            }}
+        </style>
+    </head>
+    <body>
+        <div class='email-container'>
+            <div class='email-header'>
+                <h2>IronGym Employee Password</h2>
+            </div>
+            <div class='email-content'>
+                <p>Dear Employee,</p>
+                <p>Welcome to the IronGym team! We are excited to have you on board. Your login password is provided below:</p>
+                <p class='password'>{password}</p>
+                <p>Please use this password to log in along with the email address you provided.</p>
+                <p>If you did not expect this email, please contact our support team immediately.</p>
+                <p>Best regards,<br>IronGym Team</p>
+            </div>
+            <div class='email-footer'>
+                &copy; 2024 IronGym. All rights reserved.
+            </div>
+        </div>
+    </body>
+    </html>";
             message.IsBodyHtml = true;
 
-
-            message.Subject = "IronGym Employee Password";
+            message.Subject = "Your IronGym Employee Password";
             return message;
         }
+
         private static SmtpClient PrepareSending()
         {
             SmtpClient smtp = new SmtpClient("smtp.gmail.com");
