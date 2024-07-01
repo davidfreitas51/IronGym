01c7a93918767cd133aff45a97e19065e674272c
David Freitas <davidjumi51@gmail.com>
2024-07-01 16:19:55 -0300

    Application tests

--- Application.Tests/Services.Tests/EmailServiceTests.cs
+++ Application.Tests/Services.Tests/EmailServiceTests.cs
@@ -21,5 +21,16 @@ namespace Application.Tests.Services.Tests
             result.Should().NotBeNullOrEmpty();
             result.Should().HaveLength(6, "because the verification code should be a six-character string");
         }
+
+        [Fact]
+        public void SendPasswordEmail_ShouldReturnPassword()
+        {
+            string email = "user@example.com";
+
+            var result = _emailService.SendPasswordEmail(email);
+
+            result.Should().NotBeNullOrEmpty();
+            result.Should().HaveLength(8, "because the verification code should be a eight-character string");
+        }
     }
 }
